6b4f5228faea6905f716dfea0c93175985b1aacd
Trond Mjøen <trond@twined.net>
2014-01-22 17:15:31 +0100

    - check for infinitescroll plugin before trying to attach

--- papermill/static/js/posts/posts.js
+++ papermill/static/js/posts/posts.js
@@ -1,23 +1,25 @@
     $(function(){
-        var $container = $('#posts');
+        if ($.infinitescroll) {
+            var $container = $('#posts');
 
-        $container.infinitescroll({
-            // selector for the paged navigation
-            navSelector  : '#page_nav',
-            // selector for the NEXT link (to page 2)
-            nextSelector : '#page_nav a',
-            // selector for all items you'll retrieve
-            itemSelector : '.post-wrapper',
-            loading: {
-                msgText: 'Loading next set of posts..',
-                finishedMsg: 'No more posts to load.',
-                img: 'http://i.imgur.com/qkKy8.gif',
-                speed: 0
-              }
-            },
-            // call Isotope as a callback
-            function( newElements ) {
+            $container.infinitescroll({
+                // selector for the paged navigation
+                navSelector  : '#page_nav',
+                // selector for the NEXT link (to page 2)
+                nextSelector : '#page_nav a',
+                // selector for all items you'll retrieve
+                itemSelector : '.post-wrapper',
+                loading: {
+                    msgText: 'Loading next set of posts..',
+                    finishedMsg: 'No more posts to load.',
+                    img: 'http://i.imgur.com/qkKy8.gif',
+                    speed: 0
+                  }
+                },
+                // call Isotope as a callback
+                function( newElements ) {
 
-            }
-        );
+                }
+            );
+        }
     });
