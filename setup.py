05c16034cea31c6d14bfc0e1c1df1dd13c3af76d
Trond Mjøen <trond@twined.net>
2014-02-23 15:03:39 +0100

    - version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='papermill',
-    version='0.2.1',
+    version='0.2.2',
     author=u'Twined',
     author_email='www.twined.net',
     packages=find_packages(),
