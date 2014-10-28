7a85844c3f75c4849e29d092974ce3be9b8fad19
Trond Mjøen <trond@twined.net>
2014-10-28 10:30:41 +0100

    - fixing tz datetime import and check
    - removes date_now context processor

--- papermill/context_processors.py
+++ papermill/context_processors.py
@@ -6,8 +6,6 @@
 # (c) Twined/Univers 2009-2014. All rights reserved.
 # ----------------------------------------------------------------------
 
-import datetime
-
 from .settings import PAPERMILL_SETTINGS
 
 
@@ -15,7 +13,3 @@ def config(request):
 
     cfg = PAPERMILL_SETTINGS
     return {'papermill_settings': cfg}
-
-
-def date_now(request):
-    return {'date_now': datetime.datetime.now()}
--- papermill/managers.py
+++ papermill/managers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
+from django.utils import timezone
 from django.db import models
 from django.utils.translation import get_language
 from .settings import PAPERMILL_SETTINGS
@@ -22,7 +22,7 @@ class PublishedPostsManager(models.Manager):
     def get_queryset(self):
         qs = super(PublishedPostsManager, self).get_queryset()
         qs = qs.filter(status__exact=self.model.PS_PUBLISHED,
-                       publish_at__lte=datetime.now())
+                       publish_at__lte=timezone.now())
         if PAPERMILL_SETTINGS['multilanguage']:
             qs = qs.filter(language=get_language())
 
